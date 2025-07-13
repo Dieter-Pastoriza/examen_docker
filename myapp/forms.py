8e5261eea2852881b32fb9f9e42cb613175af9b9
Dieter Pastoriza <dieterpastoriza01@gmail.com>
2025-07-13 20:27:28 -0300

    testfix

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,7 +3,7 @@ from .models import Project
 
 class CreateNewTask(forms.Form):
     title = forms.CharField(label="Título de tarea", max_length=200, widget=forms.TextInput(attrs={'class': 'input'}))
-    description = forms.CharField(label="Descripción de la tarea", widget=forms.Textarea(attrs={'class': 'input'}))
+    description = forms.CharField(label="Descripción de la tara", widget=forms.Textarea(attrs={'class': 'input'}))
     project = forms.ModelChoiceField(label="Proyecto", queryset=Project.objects.all(), empty_label="Selecciona un proyecto")
     
 class CreateNewProject(forms.ModelForm):
